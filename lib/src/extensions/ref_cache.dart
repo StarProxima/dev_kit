c7e45d09756ff1222ead7a9cd8a91235f89c3908
StarProxima <maks.prozorov1@yandex.ru>
2024-03-25 05:05:55 +0300

    Add listenUserChanges

--- lib/src/extensions/ref_cache.dart
+++ lib/src/extensions/ref_cache.dart
@@ -10,8 +10,35 @@ final _cacheMap2 = <String,
     ({bool isValidCache, Set<KeepAliveLink> links, Set<int> hashcodes})>{};
 
 extension RefCacheX on AutoDisposeRef {
+  void listenUserChanges() {
+    final ref = this;
+
+    // Мы не можем сразу подписаться на userChangedProvider, т.к. если повайдера никто не слушает и он обновляется,
+    // то падает исключение "...was disposed during loading state, yet no value could be emitted."
+    //
+    // Такое может произойти, например, при рефреше токена при заходе в приложение.
+    switch (ref) {
+      case AutoDisposeFutureProviderRef():
+        ref.listenSelf((prev, next) {
+          if ((prev?.hasValue ?? false) || next.hasValue) {
+            ref.watch(userChangedProvider);
+          }
+        });
+
+      case AutoDisposeAsyncNotifierProviderRef():
+        ref.listenSelf((prev, next) {
+          if ((prev?.hasValue ?? false) || next.hasValue) {
+            ref.watch(userChangedProvider);
+          }
+        });
+
+      default:
+        ref.watch(userChangedProvider);
+    }
+  }
+
   void cacheFor(Duration duration) {
-    watch(userChangedProvider);
+    listenUserChanges();
 
     final link = keepAlive();
     final timer = Timer(duration, link.close);
@@ -19,7 +46,7 @@ extension RefCacheX on AutoDisposeRef {
   }
 
   void cacheByTag(String cacheTag) {
-    watch(userChangedProvider);
+    listenUserChanges();
 
     _cacheMap[cacheTag]?.timer?.cancel();
     _cacheMap[cacheTag] ??= (timer: null, links: {});
@@ -28,7 +55,7 @@ extension RefCacheX on AutoDisposeRef {
 
   @Deprecated('Пока не работает должным образом')
   void cacheByTagFor(String cacheTag, Duration duration) {
-    watch(userChangedProvider);
+    listenUserChanges();
 
     final timer = Timer(duration, () {
       final item = _cacheMap2[cacheTag];
