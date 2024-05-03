418c940ec502d50191447fc48a5ba854b204f83d
Zarechnev Maxim <crefter1@yandex.ru>
2024-05-03 18:53:33 +0700

    Remove margin and padding

--- lib/src/widgets/toast_card.dart
+++ lib/src/widgets/toast_card.dart
@@ -132,7 +132,7 @@ class _ToastCardState extends ConsumerState<ToastCard> {
         child: GestureDetector(
           onTap: openCloseCard,
           child: Container(
-            margin: const EdgeInsets.only(bottom: 16),
+            margin: const EdgeInsets.only(bottom: 0),
             decoration: decoration ??
                 BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
@@ -148,7 +148,7 @@ class _ToastCardState extends ConsumerState<ToastCard> {
                 child: Column(
                   children: [
                     Padding(
-                      padding: horizontalPadding,
+                      padding: EdgeInsets.zero,
                       child: Row(
                         children: [
                           Icon(
