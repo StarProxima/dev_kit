41753c574f4645c8443863ab7ef3408cec73ff3e
StarProxima <maks.prozorov1@yandex.ru>
2024-05-07 14:55:52 +0300

    Add packages

---
+++ packages/api_wrap/test/api_wrap_test.dart
@@ -0,0 +1,16 @@
+import 'package:api_wrap/api_wrap.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
---
+++ packages/riverpod_validator/example/riverpod_validator_example.dart
@@ -0,0 +1,6 @@
+import 'package:riverpod_validator/riverpod_validator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
