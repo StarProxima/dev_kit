3c4991261b475c43b0f18f9aa644b6fd59a609c4
StarProxima <maks.prozorov1@yandex.ru>
2024-08-12 23:15:17 +0300

    Add "Only" postfix for getters in DurationUtilsX

--- dev_kit/lib/src/extensions/duration_utils.dart
+++ dev_kit/lib/src/extensions/duration_utils.dart
@@ -11,22 +11,23 @@
 /// ```
 extension DurationUtilsX on Duration {
   // Total number of days.
-  int get days => inDays;
+  int get daysOnly => inDays;
 
   // Number of hours that are not part of a full day.
-  int get hours => inHours % Duration.hoursPerDay;
+  int get hoursOnly => inHours % Duration.hoursPerDay;
 
   // Number of minutes that are not part of a full hour.
-  int get minutes => inMinutes % Duration.minutesPerHour;
+  int get minutesOnly => inMinutes % Duration.minutesPerHour;
 
   // Number of seconds that are not part of a full minute.
-  int get seconds => inSeconds % Duration.secondsPerMinute;
+  int get secondsOnly => inSeconds % Duration.secondsPerMinute;
 
   // Number of milliseconds that are not part of a full second.
-  int get milliseconds => inMilliseconds % Duration.millisecondsPerSecond;
+  int get millisecondsOnly => inMilliseconds % Duration.millisecondsPerSecond;
 
   // Number of microseconds that are not part of a full millisecond.
-  int get microseconds => inMicroseconds % Duration.microsecondsPerMillisecond;
+  int get microsecondsOnly =>
+      inMicroseconds % Duration.microsecondsPerMillisecond;
 }
 
 /// Adds extensions to num (ie. int & double) to make creating durations simple:
