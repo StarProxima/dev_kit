266dfb0d530f9bf6ab9e74c76c03e15cbd462d38
StarProxima <maks.prozorov1@yandex.ru>
2024-08-25 00:36:22 +0300

    Fixes

--- app_update_checker/lib/config/entity/stores/fetchers/app_store_fetcher.dart
+++ app_update_checker/lib/config/entity/stores/fetchers/app_store_fetcher.dart
@@ -4,7 +4,7 @@ import 'package:package_info_plus/package_info_plus.dart';
 
 import '../../release.dart';
 import '../store.dart';
-import 'store_release_fetcher.dart';
+import 'store_fetcher.dart';
 
 class AppStoreFetcher extends StoreFetcher {
   const AppStoreFetcher();
