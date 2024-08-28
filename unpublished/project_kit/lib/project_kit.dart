a8cccb35d458af30409dbb51b6fe808159e36aae
Zarechnev Maxim <crefter1@yandex.ru>
2024-08-28 21:54:44 +0700

    Remove fresh from export

--- unpublished/project_kit/lib/project_kit.dart
+++ unpublished/project_kit/lib/project_kit.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: directives_ordering
 
 // Packages
-export 'package:fresh_dio/fresh_dio.dart' hide RefreshToken;
 export 'package:gap/gap.dart';
 export 'package:riverpod_async_builder/riverpod_async_builder.dart';
 
