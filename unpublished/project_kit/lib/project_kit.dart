59fa47a9468da1323edf1d7ce6539169aca96518
Zarechnev Maxim <crefter1@yandex.ru>
2024-08-28 20:53:38 +0700

    Fix

--- unpublished/project_kit/lib/project_kit.dart
+++ unpublished/project_kit/lib/project_kit.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: directives_ordering
 
 // Packages
+export 'package:fresh_dio/fresh_dio.dart' hide RefreshToken;
 export 'package:gap/gap.dart';
 export 'package:riverpod_async_builder/riverpod_async_builder.dart';
 
