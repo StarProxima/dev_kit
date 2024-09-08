6da10c855da68c28419cdd0c7bcc4bf7f867358b
StarProxima <maks.prozorov1@yandex.ru>
2024-09-08 23:59:38 +0300

    Remove UpdatePlatform.custom

--- app_update_checker/lib/src/shared/update_platform.dart
+++ app_update_checker/lib/src/shared/update_platform.dart
@@ -24,8 +24,6 @@ class UpdatePlatform {
         kIsWeb ? web.platform : Platform.operatingSystem,
       );
 
-  factory UpdatePlatform.custom(String platform) => UpdatePlatform(platform);
-
   // ignore: member-ordering
   @override
   int get hashCode => platform.hashCode;
