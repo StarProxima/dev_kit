f1cab39f834fbdb80a27fe45b5c7f7bb20949d31
StarProxima <maks.prozorov1@yandex.ru>
2024-09-08 22:36:50 +0300

    Add docs for ReleaseStatus

--- app_update_checker/lib/src/models/release_status.dart
+++ app_update_checker/lib/src/models/release_status.dart
@@ -1,20 +1,26 @@
 enum ReleaseStatus {
-  // The release is required be installed by all users with older versions.
+  /// The release is required be installed by all users with older versions.
+  /// Every old release will be [broken].
   required,
 
-  // The release is outdated and is strictly recommended to be updated.
+  /// The release is outdated and is strictly recommended to be updated.
+  /// Every old release will be [deprecated].
   recommended,
 
-  // The release is available.
+  /// The release is available for install.
+  /// It can be optionally updated by users.
   active,
 
-  // The release is hidden from users.
+  /// The release is not available yet and is hidden to users.
+  /// Need it when the release is still being reviewed or has not been shipped yet.
   inactive,
 
-  // The release is outdated and is strictly recommended to be updated.
+  /// The release is outdated and is strictly recommended to be updated.
+  /// For this release, any new release will be [recommended].
   deprecated,
 
-  // The release has critical bugs and requires an update.
+  /// The release has critical bugs and requires an update.
+  /// For this release, any new release will be [required].
   broken;
 
   static ReleaseStatus? parse(String? str) => List<ReleaseStatus?>.of(values).firstWhere(
