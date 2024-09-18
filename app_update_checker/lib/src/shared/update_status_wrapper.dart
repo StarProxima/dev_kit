6680059d8c115aea93fbb6fd3502019b66756e2e
StarProxima <maks.prozorov1@yandex.ru>
2024-09-18 19:01:21 +0300

    UpdateSettings

--- app_update_checker/lib/src/shared/update_status_wrapper.dart
+++ app_update_checker/lib/src/shared/update_status_wrapper.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/foundation.dart';
+
+import '../parser/models/release_settings_config.dart';
+import 'update_alert_type.dart';
 import 'update_status.dart';
 
 class UpdateStatusWrapper<T> {
@@ -24,4 +28,84 @@ class UpdateStatusWrapper<T> {
         UpdateStatus.recommended => recommended,
         UpdateStatus.available => available,
       };
+}
+
+class UpdateSettings {
+  final Map<String, Map<String, ReleaseSettingsConfig?>> value;
+
+  const UpdateSettings(this.value);
+
+  factory UpdateSettings.parse(
+    Map<String, dynamic> map, {
+    required ReleaseSettingsConfig? Function(Map<String, dynamic> map) parseSettings,
+  }) {
+    final value = <String, Map<String, ReleaseSettingsConfig>>{};
+
+    final typeNames = [...UpdateAlertType.values.map((e) => e.name), 'base'];
+    final isByType = map.keys.every(typeNames.contains);
+
+    if (!isByType) {
+      final settingsByStatus = _parseByStatus(value, parseSettings: parseSettings);
+
+      // Empty UpdateSettings
+      if (settingsByStatus.isEmpty) return UpdateSettings(value);
+
+      return UpdateSettings({'base': settingsByStatus});
+    }
+
+    for (final type in typeNames) {
+      final value = map[type];
+      if (value is! Map<String, dynamic>) continue;
+
+      final settingsByStatus = _parseByStatus(value, parseSettings: parseSettings);
+      if (settingsByStatus.isEmpty) continue;
+
+      value[type] = settingsByStatus;
+    }
+
+    return UpdateSettings(value);
+  }
+
+  ReleaseSettingsConfig? by({
+    required UpdateAlertType type,
+    required UpdateStatus status,
+  }) =>
+      byRaw(type: type.name, status: status.name);
+
+  ReleaseSettingsConfig? byRaw({
+    required String type,
+    required String status,
+  }) {
+    final byType = value[type] ?? value['base'];
+    final byStatus = byType?[status] ?? byType?['base'];
+
+    return byStatus;
+  }
+
+  static Map<String, ReleaseSettingsConfig> _parseByStatus(
+    Map<String, dynamic> map, {
+    required ReleaseSettingsConfig? Function(Map<String, dynamic> map) parseSettings,
+  }) {
+    final settingsByStatus = <String, ReleaseSettingsConfig>{};
+
+    final statusNames = [...UpdateStatus.values.map((e) => e.name), 'base'];
+
+    final isByStatus = map.keys.any(statusNames.contains);
+
+    if (!isByStatus) {
+      final settings = parseSettings(map);
+      if (settings == null) return {};
+
+      return {'base': settings};
+    }
+
+    for (final status in statusNames) {
+      final value = map[status];
+      final settings = parseSettings(value);
+      if (settings == null) continue;
+      settingsByStatus[status] = settings;
+    }
+
+    return settingsByStatus;
+  }
 }
