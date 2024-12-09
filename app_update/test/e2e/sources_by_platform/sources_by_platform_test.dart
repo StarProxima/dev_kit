a5165fb0fc67d7a1ec3cfabec94ac8079ff9dcc7
iamgirya <84345515+iamgirya@users.noreply.github.com>
2024-12-09 15:11:42 +0300

    Add count tests

--- app_update/test/e2e/sources_by_platform/sources_by_platform_test.dart
+++ app_update/test/e2e/sources_by_platform/sources_by_platform_test.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:app_update/src/controller/update_controller.dart';
 import 'package:app_update/src/fetcher/update_config_fetcher.dart';
 import 'package:app_update/src/shared/update_platform.dart';
+import 'package:app_update/src/shared/version_status.dart';
 import 'package:app_update/src/sources/sources.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -15,7 +16,7 @@ void main() {
   group('Sources by platform', () {
     final updateConfigFetcher = UpdateConfigFetcher.byFile(
       file: File(
-        '${Directory.current.path}\\test\\e2e\\update_controller_full_flow\\update_controller_full_flow_config.yaml',
+        '${Directory.current.path}\\test\\e2e\\sources_by_platform\\sources_by_platform_config.yaml',
       ),
     );
 
@@ -29,6 +30,28 @@ void main() {
     SharedPreferences.setMockInitialValues({});
 
     test(
+      'Check config releases count',
+      () async {
+        final controller = UpdateController(
+          updateConfigFetcher: updateConfigFetcher,
+          targetPlatform: UpdatePlatform.windows,
+        );
+
+        final result = await controller.findAllAvailableUpdates();
+        expect(result.length, 1);
+        // ignore: avoid-unsafe-collection-methods
+        final releases = result.first.config.releases;
+        expect(result.firstOrNull?.config.releases.length, 13);
+        expect(releases.where((e) => e.source.platforms.contains(UpdatePlatform.android)).length, 8);
+        expect(releases.where((e) => e.source.platforms.contains(UpdatePlatform.windows)).length, 5);
+        expect(releases.where((e) => e.source.platforms.contains(UpdatePlatform.ios)).length, 4);
+        expect(releases.where((e) => e.source.platforms.contains(UpdatePlatform.linux)).length, 1);
+        expect(result.firstOrNull?.config.sources.length, 3);
+        expect(result.firstOrNull?.config.customData?['testName'], 'e2e');
+      },
+    );
+
+    test(
       'Get all releases for Android',
       () async {
         final controller = UpdateController(
@@ -37,13 +60,12 @@ void main() {
         );
 
         final result = await controller.findAllAvailableUpdates();
-
-        // TODO тесты на release.appUpdateStatus и на updateConfig
         expect(result.length, 2);
         expect(result.firstOrNull?.release.source.type, Sources.huaweiAppGallery);
         expect(result.lastOrNull?.release.source.name, 'gitHub');
         expect(result.firstOrNull?.release.version, Version(1, 0, 4));
         expect(result.lastOrNull?.release.version, Version(1, 0, 4));
+        expect(result.firstOrNull?.appVersionStatus, VersionStatus.updatable);
       },
     );
 
@@ -60,6 +82,7 @@ void main() {
         expect(result.length, 1);
         expect(result.firstOrNull?.release.source.name, 'gitHub');
         expect(result.firstOrNull?.release.version, Version(1, 0, 5));
+        expect(result.firstOrNull?.appVersionStatus, VersionStatus.deprecated);
       },
     );
 
@@ -76,6 +99,7 @@ void main() {
         expect(result.length, 1);
         expect(result.firstOrNull?.release.source.type, Sources.appStore);
         expect(result.firstOrNull?.release.version, Version(1, 0, 4));
+        expect(result.firstOrNull?.appVersionStatus, VersionStatus.updatable);
       },
     );
 
@@ -92,6 +116,7 @@ void main() {
         expect(result.length, 1);
         expect(result.firstOrNull?.release.source.name, 'gitHub');
         expect(result.firstOrNull?.release.version, Version(1, 0, 1));
+        expect(result.firstOrNull?.appVersionStatus, VersionStatus.updatable);
       },
     );
   });
