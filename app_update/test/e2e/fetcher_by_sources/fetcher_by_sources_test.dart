6d2ae021c067aad00d31b9f8995c98f733895f8e
iamgirya <84345515+iamgirya@users.noreply.github.com>
2024-12-09 15:18:54 +0300

    Add test for updateSettings and updateText in UpdateController

--- app_update/test/e2e/fetcher_by_sources/fetcher_by_sources_test.dart
+++ app_update/test/e2e/fetcher_by_sources/fetcher_by_sources_test.dart
@@ -1,7 +1,13 @@
-// ignore_for_file: avoid-long-functions, prefer-moving-to-variable
+// ignore_for_file: avoid-long-functions, prefer-moving-to-variable, avoid-missing-enum-constant-in-map
 
 import 'package:app_update/src/controller/update_controller.dart';
+import 'package:app_update/src/linker/models/release_settings_data.dart';
+import 'package:app_update/src/linker/models/update_settings_data_container.dart';
+import 'package:app_update/src/linker/models/update_text_data.dart';
+import 'package:app_update/src/linker/models/update_text_data_container.dart';
+import 'package:app_update/src/shared/update_alert_type.dart';
 import 'package:app_update/src/shared/update_platform.dart';
+import 'package:app_update/src/shared/version_status.dart';
 import 'package:app_update/src/sources/sources.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -47,7 +53,29 @@ void main() {
     test(
       'Fetch test app for Ios',
       () async {
+        const testReminderPeriod = Duration(days: 123);
+        const testTitle = 'lolkek';
         final controller = UpdateController(
+          updateSettings: const UpdateSettingsDataContainer(
+            {
+              UpdateAlertTypeBase.base: {
+                VersionStatusBase.base: UpdateSettingsData(
+                  reminderPeriod: testReminderPeriod,
+                ),
+              },
+            },
+          ),
+          updateText: UpdateTextDataContainer(
+            {
+              const Locale('base'): {
+                UpdateAlertTypeBase.base: {
+                  VersionStatusBase.base: const UpdateTextData(
+                    title: testTitle,
+                  ),
+                },
+              },
+            },
+          ),
           targetPlatform: UpdatePlatform.ios,
           targetSourceName: Sources.appStore.name,
         );
@@ -64,6 +92,9 @@ void main() {
           result.release.source.url.toString(),
           'https://apps.apple.com/en/app/%D0%BA%D1%83%D0%B1-%D1%80%D0%B0%D1%81%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5/id6448701011?uo=4',
         );
+
+        expect(result.release.settings.getDefault().reminderPeriod, testReminderPeriod);
+        expect(result.release.text.getDefault().title, testTitle);
       },
     );
   });
