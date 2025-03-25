669f6e7322b53faebf30d324d8b3884a7a69511c
Zarechnev Maxim <crefter1@yandex.ru>
2025-03-25 14:47:04 +0700

    Fix CardTheme

--- unpublished/app_logger/lib/src/extensions/theme_data_ext.dart
+++ unpublished/app_logger/lib/src/extensions/theme_data_ext.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 extension ThemeDataExt on ThemeData {
   /// Return ThemeData with default values of [cardTheme] fields
-  ThemeData copyWithDefaultCardTheme(CardThemeData defaultCardTheme) {
+  ThemeData copyWithDefaultCardTheme(CardTheme defaultCardTheme) {
     return copyWith(
       cardTheme: cardTheme.copyWith(
         clipBehavior: cardTheme.clipBehavior ?? defaultCardTheme.clipBehavior,
