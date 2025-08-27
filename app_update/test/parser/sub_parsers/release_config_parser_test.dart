7560c3a1794f58f5f74016baed7342d9de939b9f
StarProxima <34741787+StarProxima@users.noreply.github.com>
2025-08-27 17:50:51 +0300

    Update release_config_parser_test.dart

--- app_update/test/parser/sub_parsers/release_config_parser_test.dart
+++ app_update/test/parser/sub_parsers/release_config_parser_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid-unsafe-collection-methods, prefer-first, prefer-moving-to-variable
+
 import 'package:app_update/app_update.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:yaml/yaml.dart';
@@ -42,8 +44,9 @@ void main() {
       expect(result?.sources?[0].platforms, isNotNull);
       expect(result?.sources?[0].platforms?[0].platformName.name, 'android');
       expect(
-          result?.sources?[0].platforms?[0].releaseOverride?.version.toString(),
-          '1.2.4');
+        result?.sources?[0].platforms?[0].releaseOverride?.version?.toString(),
+        '1.2.4',
+      );
       expect(result?.sources?[1].sourceName.name, 'appStore'.toLowerCase());
       expect(result?.contentRules, isNotNull);
       expect(result?.settingsRules, isNotNull);
@@ -81,9 +84,11 @@ void main() {
       final map = Map<String, dynamic>.from(loadYaml(yamlStr));
       final result = parser.parse(map);
       expect(result, isA<ReleaseConfig>());
-      expect(result?.sources?[0].releaseOverride?.version.toString(), '0.2.1');
-      expect(result?.sources?[0].releaseOverride?.date,
-          equals(DateTime(2025, 10, 10, 12)));
+      expect(result?.sources?[0].releaseOverride?.version?.toString(), '0.2.1');
+      expect(
+        result?.sources?[0].releaseOverride?.date,
+        equals(DateTime(2025, 10, 10, 12)),
+      );
     });
 
     test('Парсинг null возвращает null', () {
@@ -92,8 +97,10 @@ void main() {
     });
 
     test('Ошибка при неверном типе входных данных', () {
-      expect(() => parser.parse('not a map'),
-          throwsA(isA<ParseConfigException>()));
+      expect(
+        () => parser.parse('not a map'),
+        throwsA(isA<ParseConfigException>()),
+      );
       expect(() => parser.parse(123), throwsA(isA<ParseConfigException>()));
       expect(() => parser.parse([]), throwsA(isA<ParseConfigException>()));
     });
