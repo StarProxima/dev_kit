9b785dbf827fdfe347238494f6f3d7e0de08a5f3
iamgirya <84345515+iamgirya@users.noreply.github.com>
2025-11-28 22:26:26 +0300

    Rename update_inker to update_linker

--- app_update/lib/src/controller/update_controller_impl.dart
+++ app_update/lib/src/controller/update_controller_impl.dart
@@ -8,7 +8,7 @@ import 'package:url_launcher/url_launcher.dart';
 import '../fetcher/update_config_fetcher.dart';
 import '../fetcher/update_config_fetcher_coordinator.dart';
 import '../fetcher/update_config_source_fetcher.dart';
-import '../linker/update_inker.dart';
+import '../linker/update_linker.dart';
 import '../models/release/update.dart';
 import '../models/release/update_data.dart';
 import '../models/update_result/update_result.dart';
